387f8f3a5ebcab31c38aa64dccd58c34d9ff2391
mina gerges <minagerges4@gmail.com>
2020-02-08 17:50:10 +0200

    new item and itemDAO

--- TPs/TP02/src/main/java/com/yaps/petstore/domain/Item.java
+++ TPs/TP02/src/main/java/com/yaps/petstore/domain/Item.java
@@ -5,10 +5,84 @@
  */
 package com.yaps.petstore.domain;
 
+import com.yaps.petstore.exception.CheckException;
+import java.io.Serializable;
+
 /**
  *
  * @author mina2
  */
-public class Item {
+public class Item implements Serializable {
+
+    String _id = null;
+    String _name = null;
+    Double _unitCost = null;
+    Product _product = null;
+
+    public Item(String id) {
+        _id = id;
+    }
+
+    public Item(String id, String name, Double unitCost, Product _product) {
+        _id = id;
+        _name = name;
+        _unitCost = unitCost;
+    }
+
+    public void setId(String id) {
+        _id = id;
+    }
+
+    public void setName(String name) {
+        _name = name;
+    }
+
+    public void setUnitCost(Double unitCost) {
+        _unitCost = unitCost;
+    }
+
+    public String getId() {
+        return _id;
+    }
+
+    public String getName() {
+        return _name;
+    }
+
+    public Double getUnitCost() {
+        return _unitCost;
+    }
+
+    public void setProduct(Product product) {
+        _product = product;
+    }
     
+    public Product getProduct(){
+        return _product;
+    }
+
+    public void checkData() throws CheckException {
+        if (_id == null || "".equals(_id)) {
+            throw new CheckException("Invalid item id");
+        }
+        if (_name == null || "".equals(_name)) {
+            throw new CheckException("Invalid item name");
+        }
+        if (_unitCost == null || "".equals(_unitCost)) {
+            throw new CheckException("Invalid item price");
+        }
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer buf = new StringBuffer();
+        buf.append("\n\tCategory {");
+        buf.append("\n\t\tId=").append(_id);
+        buf.append("\n\t\tName=").append(_name);
+        buf.append("\n\t\tPrice=").append(_unitCost);
+        buf.append("\n\t\tProduct Id=").append(_product.getId());
+        buf.append("\n\t\tProduct name=").append(_product.getName());
+        buf.append("\n\t}");
+        return buf.toString();
+    }
 }
--- TPs/TP02/src/main/java/com/yaps/petstore/persistence/ItemDAO.java
+++ TPs/TP02/src/main/java/com/yaps/petstore/persistence/ItemDAO.java
@@ -5,10 +5,103 @@
  */
 package com.yaps.petstore.persistence;
 
+import com.yaps.petstore.domain.Item;
+import com.yaps.petstore.exception.CheckException;
+import com.yaps.petstore.exception.DuplicateKeyException;
+import com.yaps.petstore.exception.ObjectNotFoundException;
+import java.util.Collection;
+
 /**
  *
  * @author mina2
  */
-public class ItemDAO {
-    
+public final class ItemDAO extends DataAccessObject {
+
+    // ======================================
+    // =             Attributes             =
+    // ======================================
+    private static final String HASHTABLE_FILE_NAME = "persistentItem.ser";
+
+    // ======================================
+    // =            Constructors            =
+    // ======================================
+    public ItemDAO() {
+        super(HASHTABLE_FILE_NAME);
+    }
+
+    // ======================================
+    // =           Business methods         =
+    // ======================================
+    /**
+     * This method gets a Item object from the HashMap.
+     *
+     * @param id Item identifier to be found in the hastable
+     * @return Item the item object with all its attributs set
+     * @throws CheckException is thrown if invalid data is found
+     * @throws ObjectNotFoundException is thrown if the item id not found in the
+     * hastable
+     */
+    public Item find(final String id) throws ObjectNotFoundException, CheckException {
+        // Checks data integrity
+        checkId(id);
+        return (Item) super.select(id);
+    }
+
+    /**
+     * This method gets All Category object from the HashMap.
+     *
+     * @return collection contains All Items
+     * @throws CheckException is thrown if invalid data is found
+     * @throws ObjectNotFoundException is thrown if the category id not found in
+     * the hastable
+     */
+    public Collection findAll() throws ObjectNotFoundException, CheckException {
+        return super.selectAll();
+    }
+
+    /**
+     * This method inserts a Item object into the HashMap and serializes the
+     * Hastable on disk.
+     *
+     * @param item Item Object to be inserted into the hastable
+     * @throws CheckException is thrown if invalid data is found
+     * @throws DuplicateKeyException is thrown when an identical object is
+     * already in the hastable
+     */
+    public void insert(final Item item) throws DuplicateKeyException, CheckException {
+        // Checks data integrity
+        item.checkData();
+        // Puts the object into the hastable
+        super.insert(item, item.getId());
+    }
+
+    /**
+     * This method updates a Item object of the HashMap and serializes the
+     * Hastable on disk.
+     *
+     * @param item Item to be updated from the hastable
+     * @throws ObjectNotFoundException is thrown if the item id not found in the
+     * hastable
+     * @throws DuplicateKeyException is thrown when an identical object is
+     * already in the hastable
+     * @throws CheckException is thrown if the item has invalid data
+     */
+    public void update(final Item item) throws ObjectNotFoundException, DuplicateKeyException, CheckException {
+        String id = item.getId();
+        checkId(id);
+        item.checkData();
+        super.update(item, id);
+    }
+
+    /**
+     * This method deletes a Item object from the HashMap and serializes the
+     * Hastable on disk.
+     *
+     * @param id Item identifier to be deleted from the hastable
+     * @throws ObjectNotFoundException is thrown if there's a persistent problem
+     */
+    public void remove(final String id) throws ObjectNotFoundException {
+        super.remove(id);
+    }
+
 }
